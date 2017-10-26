4ae01bce00643c56d90912bb3fa4ad1553a91cc3
charles.sh <charles.sh@alibaba-inc.com>
2017-10-26 14:50:45 +0800

    update

--- src/start.sh
+++ src/start.sh
@@ -1,29 +1,32 @@
 #/usr/bin/sh
-
+echo "###########   start record  #############" >> a.txt
 while 1>0
 do
     hour=$(date +%H)
     minite=$(date +%M)
     second=$(date +%S)
-    echo "################################"
-    echo $hour
-    echo $minite
-    echo $second
+    echo $hour:$minite:$second >> a.txt
     if test $hour -eq 10;then
          if test $minite -eq 30;then
-             echo "execute"
+             echo "start execute" >> a.txt
              uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
+             echo "finish execute" >> a.txt
+             sleep 60
          fi
     elif test $hour -eq 14;then
          if test $minite -eq 30;then
-             echo "execute"
+             echo "start execute" >> a.txt
              uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
+             echo "finish execute" >> a.txt
+             sleep 60
          fi
     elif test $hour -eq 14;then
          if test $minite -eq 30;then
-             echo "execute"
+             echo "start execute" >> a.txt
              uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
+             echo "finish execute" >> a.txt
+             sleep 60
          fi
     fi
-    sleep 60
+    sleep 10
 done
