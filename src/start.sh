7c9818d7bde8fb5073199f28554ef7baf60650ff
charles.sh <charles.sh@alibaba-inc.com>
2017-10-27 17:35:58 +0800

    update

--- src/start.sh
+++ src/start.sh
@@ -1,32 +1,15 @@
 #/usr/bin/sh
 echo "###########   start record  #############" >> a.txt
-while 1>0
-do
+while 1>0;do
     hour=$(date +%H)
-    minite=$(date +%M)
+    minute=$(date +%M)
     second=$(date +%S)
-    echo $hour:$minite:$second >> a.txt
-    if test $hour -eq 10;then
-         if test $minite -eq 30;then
-             echo "start execute" >> a.txt
-             uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
-             echo "finish execute" >> a.txt
-             sleep 60
-         fi
-    elif test $hour -eq 14;then
-         if test $minite -eq 30;then
-             echo "start execute" >> a.txt
-             uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
-             echo "finish execute" >> a.txt
-             sleep 60
-         fi
-    elif test $hour -eq 14;then
-         if test $minite -eq 30;then
-             echo "start execute" >> a.txt
-             uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
-             echo "finish execute" >> a.txt
-             sleep 60
-         fi
+    echo $hour:$minute:$second >> a.txt
+    if [[ $minute -eq "00" || $minute -eq "15" || $minute -eq "30" || $minute -eq "45" ]];then
+        echo "start execute" >> a.txt
+        uiautomator runtest mtbf.jar -c com.yunos.mtbf.tests.setup.SetupInput -e runner com.yunos.test.uitest.framework.AliUiTestRunner
+        echo "finish execute" >> a.txt
+        sleep 60
     fi
     sleep 10
 done
